4ddef79c4cf4ffd44a92d1e24cbff97bc7d29a1a
Clayton A. Alves <clayton.aa@gmail.com>
2014-11-12 23:15:04 -0300

    Correção do serializer json

--- serializers.py
+++ serializers.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+import datetime
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
-    if isinstance(obj, datetime):
-        serial = obj.isoformat()
-        return serial
+    if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
+        return obj.strftime("%Y-%m-%d")
+
